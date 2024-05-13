b9f936188a5a3e829f89117006388a9ca694f093
Fernando <euevcfn@gmail.com>
2024-05-13 16:17:53 -0300

    Criando entity Posty com User Aninhado

--- src/main/java/com/nilofer/workshopmongo/config/Instantiation.java
+++ src/main/java/com/nilofer/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.nilofer.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.nilofer.workshopmongo.domain.Post;
 import com.nilofer.workshopmongo.domain.User;
+import com.nilofer.workshopmongo.repository.PostRespository;
 import com.nilofer.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -15,15 +19,26 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRespository postRespository;
+
     @Override
     public void run(String... args) throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         userRepository.deleteAll();
+        postRespository.deleteAll();
 
         User maria = new User(null, "Maria", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Brown", "bob@gmail.com");
 
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo", maria);
+        Post post2 = new Post(null, sdf.parse("23/03/2019"), "Bom Dia", "Acordei feliz hj !", maria);
+
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        postRespository.saveAll(Arrays.asList(post1, post2));
 
     }
 
--- src/main/java/com/nilofer/workshopmongo/repository/CrudRepositoryU.java
+++
@@ -1,5 +0,0 @@
-package com.nilofer.workshopmongo.repository;
-
-public interface CrudRepositoryU<T1, T2> {
-
-}
